44ceeb8c5849c668375b115d00b696e68ce237f0
Connor Johnson <johnsonfconnor0603@gmail.com>
2020-01-26 13:02:15 -0800

    Initial Commit.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpHeight;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+    private SpriteRenderer mySpriteRenderer;
+    private Rigidbody2D rigidBody;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void Start()
+    {
+    }
+
+    void fixedUpdate()
+    {
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);                        
+            mySpriteRenderer.flipX = false;
+            
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            if(mySpriteRenderer != null)
+            {
+                mySpriteRenderer.flipX = true;
+            }
+        }
+
+    }
+
+    
+
+    
+}
