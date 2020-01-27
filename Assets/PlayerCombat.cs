4cfe34e0fd311daa9ebd288a8b34453a78a0272e
Connor Johnson <johnsonfconnor0603@gmail.com>
2020-01-26 16:01:30 -0800

    Save

--- Assets/PlayerCombat.cs
+++ Assets/PlayerCombat.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-   
+    public Animator animator;
 
-   
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask ememyLayers;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -17,6 +20,21 @@ public class PlayerCombat : MonoBehaviour
 
     void Attack()
     {
-
+        animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, ememyLayers);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+        }
     }
+    void OnDrawGizmoSelected()
+    {
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    
+        
+     
+
 }
